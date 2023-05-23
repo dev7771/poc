0e0ac871122706a9c78da571775a2b321a87bc60
Turan Zamanli <turanzamanli@gmail.com>
2023-05-23 09:43:33 +0400

    hotfix

--- app/Services/OpenWeatherService.php
+++ app/Services/OpenWeatherService.php
@@ -16,8 +16,8 @@ class OpenWeatherService
 
          $city = $this->geoLocationService->getLocation(request()->ip())['city'];
 
-         $weather = Cache::remember('weather_'.$city, 60, function () {
-             $response = Http::get("http://api.openweathermap.org/data/2.5/weather?q=Baku&appid=".env('OPEN_WEATHER_API_KEY'));
+         $weather = Cache::remember('weather_'.$city, 60, function () use ($city){
+             $response = Http::get("http://api.openweathermap.org/data/2.5/weather?q=".$city."&appid=".env('OPEN_WEATHER_API_KEY'));
              return  $response->json();
          });
 
